31ffbb7a2de6c5041cd48f0388176d6499bd68f8
afiimiese23 <nur24si@mahasiswa.pcr.ac.id>
2025-11-05 09:45:00 +0700

    Pertemuan8

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,7 @@
 
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
-=======
 use App\Http\Controllers\KategoriController;
->>>>>>> c6250f2 (tugas pertemuan6)
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,14 +16,6 @@ Route::get('/ketua', function () {
 
 Route::get('dashboard', [DashboardController::class, 'index'])
         ->name('dashboard');
-<<<<<<< HEAD
-=======
 
-Route::get('/kategori', [KategoriController::class, 'index'])->name('kategori.index');
-Route::get('/kategori/create', [KategoriController::class, 'create'])->name('kategori.create');
-Route::post('/kategori/store', [KategoriController::class, 'store'])->name('kategori.store');
-Route::get('/kategori/{id}/edit', [KategoriController::class, 'edit'])->name('kategori.edit');
-Route::put('/kategori/{id}', [KategoriController::class, 'update'])->name('kategori.update');
-Route::put('/kategori/{kategori_id}/update', [KategoriController::class, 'update'])->name('kategori.update');
-Route::delete('/kategori/{id}', [KategoriController::class, 'destroy'])->name('kategori.destroy');
->>>>>>> c6250f2 (tugas pertemuan6)
+Route::resource('kategori', KategoriController::class);
+Route::resource('products', \App\Http\Controllers\ProductController::class);
